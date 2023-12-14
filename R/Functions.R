4ad774f80fd16be4319d709037f1599518f1b542
Mikel Moriana Armendariz <151072409+MikelMoriana@users.noreply.github.com>
2023-12-14 13:36:02 +0100

    We create a function that checks for installed packages, installs them if not, and calls them We call the packages, download the data and call it

---
+++ R/Functions.R
@@ -0,0 +1,7 @@
+## Custom install & load function
+install_load_package <- function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, repos = "http://cran.us.r-project.org")
+  }
+  require(x, character.only = TRUE)
+}
