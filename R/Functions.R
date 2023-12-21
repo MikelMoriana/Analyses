214dd4902633567bf2c1aa3bba666be3778bda88
RaftinReyal <mikel.moriana@gmail.com>
2023-12-21 10:31:47 +0100

    I create three functions: find_plot_year, make_turf_plot_fertile and turfplot

--- R/Functions.R
+++ R/Functions.R
@@ -1,7 +1,50 @@
-## Custom install & load function
+# Install package from CRAN if necessary, and load it
 install_load_package <- function(x) {
   if (!require(x, character.only = TRUE)) {
     install.packages(x, repos = "http://cran.us.r-project.org")
   }
   require(x, character.only = TRUE)
+}
+
+# Find in what specific plot and year a species was recorded
+find_plot_year <- function(x, y) {
+  x |> 
+    filter(species == y) |> 
+    select(plotID, year) |> 
+    unique()
+}
+
+# We adjust the make_turf_function from turfmapper to include fertility instead of cover
+make_turf_plot_fertile <- function (data, grid_long, species, fertile, year, subturf, title) {
+  data <- rename(data,
+                 subturf = {{subturf}},
+                 species = {{species}},
+                 year = {{year}})
+  stopifnot(all(data$subturf %in% grid_long$subturf))
+  data <- left_join(data, grid_long, by = "subturf")
+  ggplot(data, aes(x = .data$.x, y = .data$.y, fill = {{fertile}})) + 
+    geom_tile(colour = "grey60") + facet_grid(species ~ year) + 
+    ggtitle(title) +
+    scale_x_continuous(expand = c(0, 0)) + 
+    scale_y_continuous(expand = c(0, 0)) + 
+    scale_fill_brewer(palette = "Paired") +
+    theme_bw() + 
+    theme(axis.text = element_blank(), 
+          axis.title = element_blank(), 
+          axis.ticks = element_blank(),
+          panel.spacing.y = unit(0.04, "lines"),
+          panel.grid.minor = element_blank(), 
+          strip.text.y = element_text(angle = 0))
+}
+
+turfplot <- function(x, y) {
+  x |> 
+  filter(plotID == y) |>
+  bind(x,
+       make_turf_plot_fertile(
+         data = x, 
+         year = year, species = species, fertile = fertile, subturf = subPlot,
+         title = glue::glue("Site {x$site}: plot {x$plotID}"),
+         grid_long = grid
+       ))
 }
