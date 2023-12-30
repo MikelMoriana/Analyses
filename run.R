a720a9a5d45d9d267f5e65437c1f06574dd97a20
Mikel Moriana Armendariz <151072409+MikelMoriana@users.noreply.github.com>
2023-12-30 12:25:58 +0100

    Add script= in case I want to use different scripts

--- run.R
+++ run.R
@@ -1,4 +1,4 @@
 # This is a helper script to run the pipeline.
 
-targets::tar_visnetwork()
-targets::tar_make()
+targets::tar_visnetwork(script = "_targets.R")
+targets::tar_make(script = "_targets.R")
