101f542cae2db72655e67a209c76686370d73665
Mikel Moriana Armendariz <151072409+MikelMoriana@users.noreply.github.com>
2024-01-23 15:18:42 +0100

    We work only with 4 years (remove 2022)

--- R/Ordination_analyses.R
+++ R/Ordination_analyses.R
@@ -5,6 +5,8 @@ library(vegan)
 
 community_clean <- targets::tar_read(community_clean)
 
+community_4years <- community_clean |> filter(year != 2022) # In 2022 only the center subplots were studied
+
 # We remove the columns we are not interested in, and make it wide format
 community_presence <- pivot_wider(
   community_clean,
@@ -14,15 +16,17 @@ community_presence <- pivot_wider(
   names_sort = TRUE,
 )
 
-community_species <- community_presence |> 
-  select(Ach_mil:Vio_tri) |> # We choose only the species. Doublecheck that these are the first and last species in the tibble
-  mutate(across(everything(), ~replace_na(.x, 0))) # We need 0s instead of NAs
+colnames(community_presence)
+community_species <- community_presence |>
+  select(Ach_mil:Vio_tri) |> # We choose only the species. Double-check that these are the first and last species in the tibble
+  mutate(across(everything(), ~ replace_na(.x, 0))) # We need 0s instead of NAs
 community_richness <- rowSums(community_species)
-community_info <- community_presence |> 
+community_info <- community_presence |>
   select(site:logger)
 
 # We perform the ordination analysis----
 
 set.seed(811)
 nmds_community <- metaMDS(community_species, k=2, distance="jaccard", trymax = 1000)
-# This gives an error because of missing values. It seems there is one subplot that needs to be removed (Gud_2_2 2019 subplot 9, and for 10 there is only logger, no species). Same as for the latter happens for Skj_1_3 17 2021. I cannot use the 3 letters _ 3 letters, since I remove Alc_sp. Find another solution
+# This gives an error because of missing values. It seems there is one subplot that needs to be removed (Gud_2_2 2019 subplot 9, and for 10 there is only logger, no species). Same as for the latter happens for Skj_1_3 17 2021.
+saveRDS(nmds_community)
