9e9293247eba09c0e3d04a27ed9662b562094fc8
Mikel Moriana Armendariz <151072409+MikelMoriana@users.noreply.github.com>
2024-11-11 15:31:30 +0100

    Rewritting the Run script

--- run.R
+++ run.R
@@ -1,4 +1,5 @@
 # This is a helper script to run the pipeline.
+library(targets)
 
-targets::tar_visnetwork(script = "_targets.R")
-targets::tar_make(script = "_targets.R")
+tar_visnetwork(script = "_targets.R")
+tar_make(script = "_targets.R")
